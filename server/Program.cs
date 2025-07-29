9baa19c25b742201bc701279b9d31198158152f0
Catie Lanier <hello@catielanier.ca>
2025-07-28 22:41:15 -0400

    Initializes Supabase client
    Initializes the Supabase client with URL and key from environment variables.
    
    Sets up options for auto-connecting to Realtime.
    
    Adds the Supabase package as a project dependency.

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+var options = new Supabase.SupabaseOptions
+{
+  AutoConnectRealtime = true
+};
+
+var supabase = new Supabase.Client(url!, key, options);
+
+await supabase.InitializeAsync();
 
 builder.Services.AddCors(options => 
 {
