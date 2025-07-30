03a97f76983a11d0bde58bb0f6496e83dc8d2e92
Catie Lanier <hello@catielanier.ca>
2025-07-30 16:40:58 -0400

    Enables API controllers and CORS policy
    Configures the application to use API controllers for handling requests.
    
    Enables a CORS policy specifically for development environments, allowing requests from the development client.
    
    Removes the sample weather forecast endpoint.

--- server/Program.cs
+++ server/Program.cs
@@ -13,6 +13,8 @@ await supabase.InitializeAsync();
 
 builder.Services.AddSingleton(supabase);
 
+builder.Services.AddControllers();
+
 builder.Services.AddCors(options => 
 {
     options.AddPolicy("AllowDev",
@@ -36,30 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowDev");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
