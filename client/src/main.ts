a02a347d4550de8865b891050865699fda594459
Catie Lanier <hello@catielanier.ca>
2025-08-11 21:34:24 -0400

    Initializes authentication before mounting app
    Ensures authentication state is initialized before the Vue app is mounted.
    This prevents potential race conditions and ensures that the application
    correctly reflects the user's authentication status upon initial load.

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
+import { useAuthStore } from "./stores/user";
 
-createApp(App).use(createPinia()).mount("#app");
+const app = createApp(App).use(createPinia());
+
+useAuthStore().init().finally(() => {
+  app.mount("#app")
+})
